9345fc490f6c2f4e910951098f52da2170724397
MT Ekleel <mtahsinekleel02@gmail.com>
2023-01-28 20:37:42 +0600

    Create 486a.cpp

---
+++ 486a.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+#define ld long double
+#define tc(val) if (val) { int t; cin >> t; while (t--) { solve(); } return 0;} else { solve(); return 0; }
+using namespace std;
+
+void solve() {
+    ll n;
+    cin >> n;
+    if (n % 2 == 0) {
+        cout << (n / 2) << "\n"; 
+    } else {
+        cout << (n - 1) / 2 - n << "\n";
+    }
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    tc(0);
+}
